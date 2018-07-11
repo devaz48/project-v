5c9db30013bcab42faf889c66e5eeae3284b0357
devaz48 <39666044+devaz48@users.noreply.github.com>
2018-07-11 14:18:13 +0700

    Add files via upload
    INI HANYA KEGABUTAN SEMATA

---
+++ CopyrightJS.js
@@ -0,0 +1,15 @@
+﻿
+
+var SiteBirthDay=1;
+var SiteBirthMonth= 1;
+var SiteBirthYear= 0;
+var Sitenow=new Date();
+var SiteCurrDay=Sitenow.getDate();
+var SiteCurrMonth=Sitenow.getMonth()+1;
+var SiteCurrYear=Sitenow.getFullYear();
+var SiteMyAge=SiteCurrYear-SiteBirthYear-1;
+if ((SiteBirthMonth<SiteCurrMonth) || ((SiteBirthMonth==SiteCurrMonth) && (SiteBirthDay<=SiteCurrDay))) {SiteMyAge++;}
+
+
+
+document.write(' Copyright &copy; 2013- ' +SiteMyAge+ ' Lingkakarya Media Formasi Indonesia<br>All rights reserved. ');
