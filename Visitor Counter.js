30168ab5ffb504c81ac0f7a4b95535de0272b6ad
devaz48 <39666044+devaz48@users.noreply.github.com>
2018-07-11 14:19:26 +0700

    Just Visitor Counter For Website

---
+++ Visitor Counter.js
@@ -0,0 +1,24 @@
+expireDate = new Date
+expireDate.setMonth(expireDate.getMonth()+6)
+count_num = eval(cookieVal("counter_style_Visited"))
+count_num++
+document.cookie = "counter_style_Visited="+count_num+";expires=" + expireDate.toGMTString()
+
+function cookieVal(cookieName) {
+thisCookie = document.cookie.split("; ")
+for (i=0; i<thisCookie.length; i++){
+	if (cookieName == thisCookie[i].split("=")[0]){
+		return thisCookie[i].split("=")[1]
+	}
+}
+return 0
+}
+
+function page_countvisit(){
+for (i=0;i<(7-count_num.toString().length);i++)
+document.write('<span class="counter_style">0</span>')
+for (y=0;y<(count_num.toString().length);y++)
+document.write('<span class="counter_style">'+count_num.toString().charAt(y)+'</span>')
+}
+
+/* <script type="text/javascript" lang="Eng">page_countvisit(count_num) */
